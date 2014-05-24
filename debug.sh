5462e959289f25d606ad3e9e8517abcddbecad10
iscr <xxqscr@gmail.com>
2014-05-24 17:44:36 +0800

    debug.sh

--- debug.sh
+++ debug.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-erl +P 1024000 +K true -smp disable -pa ebin -boot start_sasl -s x start   -mnesia dir '"Mnesia.nonode@nohost"'
+erl +P 1024000 +K true -smp disable -pa ebin -boot start_sasl -s x start   -mnesia dir '"./Mnesia.nonode@nohost"'
