4e51b32ca79ab150c7a21bf3a9908441f85f573f
Scrxue <xxqscr@gmail.com>
2014-07-24 01:39:21 +0800

    8.27 练习 最常见函数名 提交 遇到的问题 : map的模式匹配在R17.0下仍然有问题 改用dict 问题原因参考: http://stackoverflow.com/questions/23106543/illegal-pattern-in-map-of-erlang 解决方案参考: http://metaljoe.wordpress.com/2009/09/16/erlang-in-my-head-part-3-counting-characters/

--- src/module_test.erl
+++ src/module_test.erl
@@ -22,22 +22,26 @@ return_most_fun_of_module() ->
 
 % [{Module, [fun, fun...]}, {Module, [fun, fun...]}]
 
-% return_most_fun_name() ->
-% 	Mod_and_fun_list = mod_and_fun_list(),
-% 	L1 = [X || {_Mod, X } <- Mod_and_fun_list],
-% 	L2 = lists:flatten(L1),
-% 	count_functions(L2, #{}).
+return_most_fun_name() ->
+	Mod_and_fun_list = mod_and_fun_list(),
+	L1 = [X || {_Mod, X } <- Mod_and_fun_list],
+	L2 = lists:flatten(L1),
+	L3 = dict:to_list(same_fun_name_map(L2, dict:new())),
+	lists:sort(fun({_, N1}, {_, N2}) -> 
+		N1 > N2
+	end, L3).
 
 
-% % same_fun_name_map([], Map) ->
-% % 	Map;
 
-% % same_fun_name_map([H | T], Map) ->
-% % 	Mname = H,
-% % 	case maps:is_key(H, Map) of
-% % 		true -> same_fun_name_map(T, Map#{Mname := maps:get(H, Map) + 1});
-% % 		false -> same_fun_name_map(T, Map#{Mname => 1})
-% % 	end.
+same_fun_name_map([], Dict) ->
+	Dict;
+
+same_fun_name_map([H | T], Dict) ->
+	Dict1 = case dict:is_key(H, Dict) of
+			true -> dict:update(H, fun(Value) -> Value + 1 end, Dict);
+			false -> dict:store(H, 1, Dict)
+		end,
+	same_fun_name_map(T, Dict1).
 
 % count_functions([], X) ->
 % 	X;
@@ -47,16 +51,16 @@ return_most_fun_of_module() ->
 % count_functions([H|T], X) ->
 % 	count_functions(T, X#{H => 1}).
 
-count_characters(Str) ->
-	count_characters(Str, #{}).
+% count_characters(Str) ->
+% 	count_characters(Str, #{}).
 
-count_characters([H|T], #{ H => N } = X) ->
-	count_characters(T, X#{ H := N+1});
+% count_characters([H|T], #{ H => N } = X) ->
+% 	count_characters(T, X#{ H := N+1});
 
-count_characters([H|T], X) ->
-	count_characters(T, X#{ H => 1});
-count_characters([], X) ->
-	X.
+% count_characters([H|T], X) ->
+% 	count_characters(T, X#{ H => 1});
+% count_characters([], X) ->
+% 	X.
 
 
 mod_and_fun_list() ->
